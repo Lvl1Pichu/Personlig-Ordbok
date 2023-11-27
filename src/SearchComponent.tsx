5d2c7700341c1e1a907703b629ff5939e40ac1e5
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-11-27 13:07:57 +0100

    Audio now functional

--- src/SearchComponent.tsx
+++ src/SearchComponent.tsx
@@ -3,6 +3,7 @@ import React, { useState, ChangeEvent } from 'react';
 const SearchComponent: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [definition, setDefinition] = useState<string | null>(null);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const fetchDefinition = async (word: string) => {
@@ -11,6 +12,7 @@ const SearchComponent: React.FC = () => {
       if (!response.ok) {
         if (response.status === 404) {
           setDefinition('Word not found in the dictionary.');
+          setAudioUrl(null);
         } else {
           throw new Error('Failed to fetch the definition.');
         }
@@ -19,9 +21,14 @@ const SearchComponent: React.FC = () => {
       const data = await response.json();
       const definitions = data[0].meanings.map((meaning: any) => meaning.definitions.map((def: any) => def.definition).join('\n')).join('\n\n');
       setDefinition(definitions);
+
+      // Check if there is an audio URL and set it
+      const audio = data[0].phonetics.find((phonetic: any) => phonetic.audio);
+      setAudioUrl(audio ? audio.audio : null);
     } catch (error) {
       console.error('Error fetching definition:', error);
       setDefinition(null);
+      setAudioUrl(null);
     }
   };
 
@@ -29,6 +36,7 @@ const SearchComponent: React.FC = () => {
     if (!searchTerm.trim()) {
       setErrorMessage('Please enter a word to search.');
       setDefinition(null);
+      setAudioUrl(null);
       return;
     }
 
@@ -58,6 +66,13 @@ const SearchComponent: React.FC = () => {
         <div>
           <h3>Definition:</h3>
           <p>{definition}</p>
+        </div>
+      )}
+      {audioUrl && (
+        <div>
+          <audio controls src={audioUrl}>
+            Your browser does not support the audio element.
+          </audio>
         </div>
       )}
     </div>
